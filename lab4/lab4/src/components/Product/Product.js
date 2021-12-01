42295ed3315626dc7e7ce977abcdb11a6263515a
Sam <43417109+onepoint16@users.noreply.github.com>
2021-12-01 18:15:30 +0000

    Lab 4-1 complete
    Basic react app with 4 products that can be added and removed from a state managed basket

---
+++ lab4/lab4/src/components/Product/Product.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './Product.css';
+
+const products = [{
+    name: 'lettuce',
+    price: 1.00
+    },
+    {
+    name: 'aubergine',
+    price: 2.5,
+    },
+    {
+    name: 'tomato',
+    price: 1.99
+    },
+    {
+    name: 'cucumber',
+    price: 0.80
+    }
+    ];
+
+export default class Product extends Component {
+    state = {
+        cart: [],
+        total: 0
+        }
+
+    add = (product) => {
+        this.setState(state => ({
+        cart: [...state.cart, product],
+        }))
+    }
+
+    remove = (product) => {
+        this.setState(state => {
+            const cart = [...state.cart];
+            const productIndex = cart.findIndex(p => p.name === product.name);
+            if(productIndex < 0) {
+                return;
+            }
+            cart.splice(productIndex, 1)
+                return ({
+                cart
+            })
+        })
+    }
+
+    currencyOptions = {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }
+
+    getTotal = () => {
+        const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price, 0);
+        return total.toLocaleString(undefined, this.currencyOptions)
+    }
+
+    render() {
+        return(
+            <div className="wrapper">
+                <div>
+                    {products.map(product => (
+                    <div key={product.name}>
+                <div className="product"> <span>{product.name}</span></div>
+                <button onClick={() => this.add(product)}>Add</button>
+                <button onClick={() => this.remove(product)}>Remove</button>
+                </div>
+                ))}
+            </div>
+                <div>Number of items: {this.state.cart.length}</div>
+                <div>Total {this.getTotal()}</div>
+            </div>
+        )
+    }
+}
